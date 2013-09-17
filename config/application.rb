81731710cceed458d89aafc47918a636caa7e953
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-09-17 15:24:12 +0300

    setup

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "mongoid/railtie"
+require "mongoid-grid_fs"
+require "carrierwave/mongoid"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
