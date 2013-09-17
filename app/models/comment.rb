1ba038926d64f6bc7d6bd9d588ac3efed63e539f
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-09-17 14:36:42 +0300

    Add comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,22 @@
+class Comment
+  include Mongoid::Document
+
+  before_save :set_time, if: Proc.new { |comment| comment.has_no_time? }
+
+  field :content, type: String
+  field :created_at, type: Time
+
+  belongs_to :post
+  belongs_to :user
+
+  validates :content, :user_id, :post_id, presence: true
+
+  def has_no_time?
+    created_at == nil
+  end
+
+  def set_time
+    self.created_at = Time.now
+  end
+
+end
