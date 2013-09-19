3ef3bd044cc22bc065e336b4d909b8a5fdb10703
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-09-20 02:23:16 +0300

    Add handy methods for processing file upload better

--- app/uploaders/dataset_file_uploader.rb
+++ app/uploaders/dataset_file_uploader.rb
@@ -1,12 +1,14 @@
 # encoding: utf-8
 
 class DatasetFileUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  include CarrierWave::MimeTypes
   storage :grid_fs
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -14,7 +16,27 @@ class DatasetFileUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
 
-    asset_path("fallback/" + [version_name, "default.xls"].compact.join('_'))
+    # asset_path("fallback/" + [version_name, "default.xls"].compact.join('_'))
+    "default"
   end
+
+  def file_version version = nil
+    if version.nil?
+      self.file
+    elsif self.respond_to? version
+      self.send(version).file
+    else
+      nil
+    end
+  end
+
+  def image? file = nil
+    file = self.file if file.nil?
+    return nil if file.nil?
+
+    file.content_type.include? 'image'
+  end
+
+  process :set_content_type
 
 end
