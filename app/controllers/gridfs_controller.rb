3889b0ac15aa354429c8ed9df5d85da70179ffdc
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-09-20 02:22:33 +0300

    Rewrite GridFS controller to handle file uploads better...limit still 16MB :-(

--- app/controllers/gridfs_controller.rb
+++ app/controllers/gridfs_controller.rb
@@ -1,15 +1,31 @@
 class GridfsController < ActionController::Metal
 
   def serve
-    gridfs_path = env["PATH_INFO"].gsub("upload/grid/", "")
+
+    dataset_file_id = params[:fid]
+
     begin
-      gridfs_file = Mongoid::GridFS[gridfs_path]
-      self.response_body = gridfs_file.data
+      dataset = Dataset.find(dataset_file_id)
+
+      if dataset.nil? || dataset.attachment.nil?
+        raise Exception.new("Unable to find dataset document with id #{doc_id}")
+      end
+
+      filename = Pathname.new(env['PATH_INFO']).basename.to_s
+      version = filename.include?('_') ? filename.split('_')[0] : nil
+
+      gridfs_file = dataset.attachment.file_version(version)
+      gridfs_file = dataset.attachment.file if gridfs_file.nil? || gridfs_file.length.nil?
+
+      self.headers['Content-Length'] = gridfs_file.content_length.to_s
+      self.headers['Expires'] = 1.year.from_now.httpdate
+      self.response_body = gridfs_file.read
       self.content_type = gridfs_file.content_type
-    rescue
+    rescue Exception => e
       self.status = :file_not_found
       self.content_type = 'text/plain'
-      self.response_body = ''
+      self.response_body = e.message
     end
   end
+
 end
