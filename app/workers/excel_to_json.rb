58f9000183ade48c41c8694a9c17530c254fa104
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-24 02:39:30 +0300

    a few things to ignore and tweaks to worker

--- app/workers/excel_to_json.rb
+++ app/workers/excel_to_json.rb
@@ -6,13 +6,6 @@ class ExcelToJson
 
   def self.perform(dataset_id, tempfile, chart_type)
     puts "---- ***** ---- *** ---- "
-    # dataset = Dataset.find dataset_id
-    # puts "#{dataset.name} processing occurring..."
-    # uri = URI.parse('http://e2j.opendevdata.ug/upload')
-    # request = Net::HTTP.post_form(uri, {'excel' => tempfile.path })
-    # TODO -> test if request.body is json response! (or provide error)
-    # dataset.update_attribute(:data_extract, request.body)
-
     conn = Faraday.new url: 'http://e2j.opendevdata.ug/upload/' do |faraday|
       faraday.request :multipart
       faraday.adapter :net_http
@@ -23,6 +16,10 @@ class ExcelToJson
                 chart_type: chart_type }
 
     response = conn.post 'http://e2j.opendevdata.ug/upload/', payload
+    # dataset = Dataset.find dataset_id
+    # dataset.update_attribute(:data_extract, request.body)
+    # puts "#{dataset.name} processed..."
     response.body  # returns a response in form of json
+
   end
 end
