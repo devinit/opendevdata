a20087853037504d96b718de326c79464e431a53
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-23 20:38:23 +0300

    a field to define a chart type... no time to put crazy AI

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -25,7 +25,9 @@ class DatasetsController < ApplicationController
     @dataset = Dataset.create dataset_params
     if @dataset.save
       # send file for processing
-      Resque.enqueue(ExcelToJson, @dataset.id.to_s, dataset_params['attachment'].tempfile)
+      Resque.enqueue(ExcelToJson, @dataset.id.to_s,
+        dataset_params['attachment'].tempfile,
+        dataset_params['chart_type'])
       redirect_to @dataset, notice: "You have successfully uploaded a dataset"
     else
       flash[:alert] = "The dataset you have could not be uploaded."
