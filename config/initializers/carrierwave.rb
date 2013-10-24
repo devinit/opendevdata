566760c0a0ebfeb0bc0a1163bb4ad371022204a5
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-24 03:22:03 +0300

    cleanup

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,4 @@
 CarrierWave.configure do |config|
   config.storage = :grid_fs
   config.grid_fs_access_url = "/fs"
-  # config.root = Rails.root.join('tmp')
-
-  # if Rails.env.production?
-  #   config.grid_fs_database = ENV["MONGOID_DATABASE"]
-  #   config.grid_fs_host = ENV["MONGOID_HOST"]
-  #   config.grid_fs_port = ENV["MONGOID_PORT"]
-  #   config.grid_fs_username = ENV["MONGOID_USERNAME"]
-  #   config.grid_fs_password = ENV["MONGOID_PASSWORD"]
-  # end
 end
