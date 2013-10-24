90c0bd1b8795d55d96ccdf3a616528a3b99d3090
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-24 03:14:49 +0300

    relaxed settings, https://github.com/carrierwaveuploader/carrierwave-mongoid/blob/master/lib/carrierwave/mongoid.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
   config.grid_fs_access_url = "/fs"
   # config.root = Rails.root.join('tmp')
 
-  if Rails.env.production?
-    config.grid_fs_database = ENV["MONGOID_DATABASE"]
-    config.grid_fs_host = ENV["MONGOID_HOST"]
-    config.grid_fs_port = ENV["MONGOID_PORT"]
-    config.grid_fs_username = ENV["MONGOID_USERNAME"]
-    config.grid_fs_password = ENV["MONGOID_PASSWORD"]
-  end
+  # if Rails.env.production?
+  #   config.grid_fs_database = ENV["MONGOID_DATABASE"]
+  #   config.grid_fs_host = ENV["MONGOID_HOST"]
+  #   config.grid_fs_port = ENV["MONGOID_PORT"]
+  #   config.grid_fs_username = ENV["MONGOID_USERNAME"]
+  #   config.grid_fs_password = ENV["MONGOID_PASSWORD"]
+  # end
 end
