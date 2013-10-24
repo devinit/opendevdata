b91b378305de1e912bc9f01e3bf92024e222ec2d
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-24 04:10:14 +0300

    disable confirmation until we get mailgun working :p

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+    :rememberable, :trackable, :validatable
 
   ## More user information
   field :first_name,         type: String
