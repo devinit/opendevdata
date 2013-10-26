da3571dc5101b0d290642a28e20213d369ecc321
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-26 04:53:58 +0300

    formalized a simple usecase for a fair number of excel files

--- app/workers/excel_to_json.rb
+++ app/workers/excel_to_json.rb
@@ -4,22 +4,29 @@ require 'faraday'
 class ExcelToJson
   @queue = :excel_to_json
 
-  def self.perform(dataset_id, tempfile, chart_type)
+  def self.perform dataset_id, tempfile_path, chart_type, file_name
     puts "---- ***** ---- *** ---- "
     conn = Faraday.new url: 'http://e2j.opendevdata.ug/upload/' do |faraday|
       faraday.request :multipart
       faraday.adapter :net_http
     end
-
-    payload = { upload_file: Faraday::UploadIO.new(tempfile.path,
+    puts "-- connection object created..."
+    payload = { upload_file: Faraday::UploadIO.new(tempfile_path,
                                                    'application/vnd.ms-excel'),
                 chart_type: chart_type }
+    puts "--** Payload prepared... posting to connection"
 
     response = conn.post 'http://e2j.opendevdata.ug/upload/', payload
-    # dataset = Dataset.find dataset_id
-    # dataset.update_attribute(:data_extract, request.body)
-    # puts "#{dataset.name} processed..."
-    response.body  # returns a response in form of json
 
+    if response.status == 200  #HTTP OK
+      dataset = Dataset.find dataset_id
+      puts "#{dataset.name} processed"
+      json_response = response.body
+      puts "Updating dataset ***"
+      dataset.data_extract = JSON.parse json_response  # parse json response
+      dataset.save
+    else
+      puts "Failed"
+    end
   end
 end
