8314642e4921f59d770acce03a24ddae25c5abff
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-04 06:25:36 +0300

    js and css for different cool plugins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
 //= require masonry/jquery.masonry
-//= require jquery.turbolinks
 //= require ckeditor/init
 //= require highcharts
 //= require highcharts-more
