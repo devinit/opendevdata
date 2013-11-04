0d86875ff8b570928cc3d5ed1bfd941601062278
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-04 06:26:11 +0300

    persist time values

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -2,6 +2,8 @@ class Dataset
   include Mongoid::Document
   include Mongoid::Slug
 
+  before_save :set_time
+
   field :name, type: String
   slug :name
   field :description, type: String
@@ -14,6 +16,8 @@ class Dataset
   field :title, type: String
   field :sub_title, type: String
   field :data_units, type: String  # suffix for tooltip
+  field :created_at, type: Time
+  field :edited_at, type: Time
 
   mount_uploader :attachment, DatasetFileUploader
 
@@ -22,4 +26,13 @@ class Dataset
   validates :name, :description, :attachment, :chart_type, :sub_title, :title, presence: true
 
   embeds_many :comments
+
+  protected
+    def set_time
+      if created_at.nil?
+        self.created_at = Time.now
+      else
+        self.edited_at = Time.now  # update the edited time
+      end
+    end
 end
