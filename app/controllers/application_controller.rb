f2286a1eeb92fa8774bfbc242b24370c13f69922
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-05 07:03:17 +0300

    cancan rescue option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
   protected
   def configure_permited_parameters
     # parameters to allow during `sign_in`
