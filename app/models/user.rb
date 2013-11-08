3deac97fbb71224535bf213156d557b1bdf8ba65
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-08 09:25:55 +0300

    facebook selfie

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,4 +52,26 @@ class User
   validates :first_name, :last_name, presence: true
 
   has_many :posts, dependent: :delete
+
+  def self.find_for_facebook_oauth auth, signed_in_resource=nil
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    unless user
+      user = User.create(first_name:auth.extra.raw_info.first_name,
+        last_name: auth.extra.raw_info.last_name,
+        uid: auth.uid,
+        email: auth.info.email,
+        password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
+
+  def self.new_with_session params, session
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
+
 end
