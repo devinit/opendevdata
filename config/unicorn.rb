8f2cfcebd661bead24de1ae053339aea4e053210
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-15 12:54:59 +0300

    comment out

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,6 @@ timeout 30
 
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
-before_exec do |_|
-  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
-end
+#before_exec do |_|
+#  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+#end
