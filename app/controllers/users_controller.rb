6b2a3e36b6cac07f68ac541b7fe9cbf5f109446c
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-17 18:22:32 +0300

    more user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+
+  def destroy
+    if current_user.is_admin?
+      @user = User.find params[:id]
+      name = @user.full_name
+      @user.destroy
+      redirect_to users_path, notice: "You have successfully deleted #{name}"
+    else
+      redirect_to users_path, alert: "You cannot delete this user"
+    end
+  end
+
 end
