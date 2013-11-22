db8503988ba905e764e741c7cf1cbaa07d129ef1
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-22 20:02:33 +0300

    setup api file

---
+++ app/api/opendata.rb
@@ -0,0 +1,40 @@
+module Opendata
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'opendata'
+    format :json
+
+    helpers do
+      def current_user
+        @current_user ||= User.authorize!(env)
+      end
+
+      def authenticate!
+        error!('401 Unauthorized', 401) unless current_user
+      end
+    end
+
+    resources :datasets do
+
+      desc "Return all datasets"
+      get :all do
+        Dataset.limit(10)
+      end
+
+      desc "Return a single dataset"
+      params do
+        requires :id, type: String, desc: "Dataset id"
+      end
+      route_param :id do
+        get do
+          Dataset.find params[:id]
+        end
+      end
+
+      # desc "Create a dataset"
+      # params do
+      #   # TODO
+      # end
+
+    end
+  end
+end
