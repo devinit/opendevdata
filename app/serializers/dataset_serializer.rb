3b0f937de47ebb420ca3241de33f5e4540e9c848
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-11-27 16:57:56 +0300

    cleanup serializers

--- app/serializers/dataset_serializer.rb
+++ app/serializers/dataset_serializer.rb
@@ -1,3 +1,22 @@
 class DatasetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :data_extract
+  attributes :id, :name, :data_extract, :url
+
+  #TODO -> test whether we need to include comments on data (i.e. what is the need by
+  # by API consumers)
+  # has_many :comments
+  # belongs_to :organization # reference to the creating organization
+
+  # The need for editing datasets programmatically through APIs has to be determined
+  def attributes
+    data = super
+    if current_user.is_admin?  # scope to admin TODO-> decide other scopes
+      data[:edit_url] = edit_dataset_url(object)
+      data[:destroy_url] = dataset_url(object)
+    end
+    data
+  end
+
+  def url
+    dataset_url object
+  end
 end
