c9fbe70a09644bd148b3e4233a601117f7a95d49
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-12-05 20:32:30 +0300

    moving things around

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
-    :rememberable, :trackable, :validatable, :confirmable
+    :rememberable, :trackable, :validatable #, :confirmable
 
   devise :omniauthable, omniauth_providers: [:facebook, :twitter]
 
