80dd430ff6cbf838f584cdca0de1b45b9d590e3a
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-12-07 02:34:55 +0300

    allow people to see datasets without actually having to login

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,6 +1,6 @@
 class DatasetsController < ApplicationController
   layout "ordinary_application"
-  before_filter :authenticate_user!, except: :index
+  before_filter :authenticate_user!, except: [:index, :show]
   before_filter :get_dataset, only: [:edit, :show, :update, :destroy]
 
   def get_dataset
