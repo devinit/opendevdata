07202b805afa1dbfbd410a4e89ec710525e05f4c
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-12-09 02:51:38 +0300

    when signed in display something else

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,20 @@
 class PagesController < ApplicationController
+  layout :page_layout
   def index
     @recent_posts = Post.scoped(limit: 3)
     @recent_datasets = Dataset.scoped(limit: 3)
   end
 
   def about
+  end
+
+  private
+  def page_layout
+    if signed_in?
+      "ordinary_application"
+    else
+      "application"
+    end
   end
 
 end
