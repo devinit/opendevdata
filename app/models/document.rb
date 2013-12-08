0a8886026d6b9f7005c3ceae4f88c70c48f0c5b6
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-12-09 02:52:49 +0300

    set time before save

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,16 @@
 class Document
   include Mongoid::Document
+  before_save :set_time
   field :name, type: String
   field :description, type: String
+  field :uploaded_on, type: Time
 
   validates :name, :description, presence: true
 
   mount_uploader :upload, DocumentUploader
+
+  private
+  def set_time
+    self.uploaded_on = Time.now if uploaded_on.nil?
+  end
 end
