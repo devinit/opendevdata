ca48d2d500ab7fca18c10bb4dbf94fcc7e506e56
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-12-09 02:50:47 +0300

    some methods to control how documents are manipulated

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,32 @@
 class DocumentsController < ApplicationController
+  layout "ordinary_application"
+
+  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :get_document, only: [:edit, :show, :update, :destroy]
+
+  def get_document
+    @document = Document.find(slug=params[:id])  # we are using slugs!
+    rescue Mongoid::Errors::DocumentNotFound
+      flash[:alert] = "The document you were looking for could not be found."
+      redirect_to documents_path
+  end
+
   def index
+    @documents = Document.desc(:uploaded_on).page(params[:page])
+    render layout: "old"
   end
 
   def new
+    @document = Document.new
+    logger.debug "Document >>> #{@document}"
   end
 
   def edit
   end
+
+  private
+  def document_params
+    params.require(:document).permit(:name, :description, :upload)
+  end
+
 end
