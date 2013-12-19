24a3e2a0ea0acb21250abf00564f788cdf54023d
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-12-20 01:49:59 +0300

    choose the right Model

--- app/controllers/gridfs_controller.rb
+++ app/controllers/gridfs_controller.rb
@@ -2,20 +2,29 @@ class GridfsController < ActionController::Metal
 
   def serve
 
-    dataset_file_id = params[:fid]
+    file_id = params[:fid]
+
+    if params[:model] == "document"
+      model = Document
+    elsif params[:model] == "dataset"
+      model = Dataset
+    else
+      model = nil
+    end
 
     begin
-      dataset = Dataset.find(dataset_file_id)
 
-      if dataset.nil? || dataset.attachment.nil?
+      model_instance = model.find(file_id)
+
+      if model_instance.nil? || model_instance.attachment.nil?
         raise Exception.new("Unable to find dataset document with id #{doc_id}")
       end
 
       filename = Pathname.new(env['PATH_INFO']).basename.to_s
       version = filename.include?('_') ? filename.split('_')[0] : nil
 
-      gridfs_file = dataset.attachment.file_version(version)
-      gridfs_file = dataset.attachment.file if gridfs_file.nil? || gridfs_file.length.nil?
+      gridfs_file = model_instance.attachment.file_version(version)
+      gridfs_file = model_instance.attachment.file if gridfs_file.nil? || gridfs_file.length.nil?
 
       self.headers['Content-Length'] = gridfs_file.content_length.to_s
       self.headers['Expires'] = 1.year.from_now.httpdate
