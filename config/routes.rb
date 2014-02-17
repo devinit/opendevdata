63aa26726b259395ff5d9a76287deea724c3a528
Victor Miclovich <vicmiclovich@gmail.com>
2014-02-17 01:14:27 -0500

    fix infinite redirect error on signin/signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Opendataportal::Application.routes.draw do
   get "about", to: 'pages#about', as: :about
   get "developer", to: 'pages#developer', as: :developer
 
+  devise_for :users
+
+  devise_scope :user do
+    get "register", to: "devise/registrations#new", as: :register
+    get "login", to: "devise/sessions#new", as: :login
+    get "logout", to: "devise/sessions#destroy", as: :logout
+  end
+
   authenticate :user, lambda { |u| u.is_admin? } do
     mount Sidekiq::Web => '/sidekiq'
     resources :users
@@ -13,13 +21,6 @@ Opendataportal::Application.routes.draw do
     match 'users/:id/make_admin', to: 'users#make_admin', as: :make_admin, via: :post
     match 'users/:id/remove_admin', to: 'users#remove_admin', as: :remove_admin, via: :post
     get 'admin/', to: 'pages#admin', as: :admin
-  end
-
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
-  devise_scope :user do
-    get "register", to: "devise/registrations#new", as: :register
-    get "login", to: "devise/sessions#new", as: :login
-    get "logout", to: "devise/sessions#destroy", as: :logout
   end
 
   get '/fs/uploads/:model/:field/:fid/:handle' => 'gridfs#serve', handle: /.*/
