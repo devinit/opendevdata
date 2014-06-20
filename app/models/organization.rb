e4ed85d1b89cac7431ed4e852b1feca4196192e1
miclovich <vicmiclovich@gmail.com>
2014-06-20 07:02:28 +0300

    removed

--- app/models/organization.rb
+++
@@ -1,9 +0,0 @@
-class Organization
-  include Mongoid::Document
-  field :name, type: String
-  field :location, type: String
-
-  # an organization or "workspace" has many datasets
-  # embeds_many :datasets, validate: false, dependent: :nullify
-  # embeds_many :documents, validate: false, dependent: :nullify
-end
