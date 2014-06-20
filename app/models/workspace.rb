0eb55d277ace55a32fd36c26ab44bbf0ed0a3f3d
miclovich <vicmiclovich@gmail.com>
2014-06-20 07:01:21 +0300

    introducing the workspace model

---
+++ app/models/workspace.rb
@@ -0,0 +1,16 @@
+class Workspace
+  include Mongoid::Document
+  field :organization_name, type: String
+  field :location, type: String
+  field :description, type: String
+
+  has_many :memberships
+  has_many :datasets
+
+  validates :organization_name, :location, :description, presence: true
+
+  def users
+    # get users that are members of this workspace
+    User.in id: memberships.map(&:user_id)
+  end
+end
