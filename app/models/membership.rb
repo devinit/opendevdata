dff3ee9d19342e8f666e4553e64e236ec74ab4f3
miclovich <vicmiclovich@gmail.com>
2014-06-20 07:01:46 +0300

    introducing the membership model

---
+++ app/models/membership.rb
@@ -0,0 +1,9 @@
+class Membership
+  include Mongoid::Document
+  field :admin, type: Boolean, default: false
+  field :approved, type: Boolean, default: false
+  belongs_to :user, dependent: :destroy
+  belongs_to :workspace, dependent: :destroy
+
+  validates_uniqueness_of :user_id
+end
