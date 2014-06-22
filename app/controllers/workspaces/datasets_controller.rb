8bed142e55077921498f9ca530547e38ecf32a0b
miclovich <vicmiclovich@gmail.com>
2014-06-22 22:46:14 +0300

    cleanup our logic

--- app/controllers/workspaces/datasets_controller.rb
+++ app/controllers/workspaces/datasets_controller.rb
@@ -3,7 +3,18 @@ class Workspaces::DatasetsController < ApplicationController
   before_filter :get_workspace
 
   def index
-    @dataset = @workspace.datasets.all
+    @datasets = @workspace.datasets.all
+
+    @tags = []
+    @datasets.tags.each do |tag|
+      @tags << tag
+    end
+
+    if params[:search].nil?
+      @datasets = @datasets.desc(:created_at).page(params[:page])
+    else
+      @datasets = @datasets.search(params[:search]).uniq.delete_if { |dataset| dataset.nil? or dataset.empty? }
+    end
   end
 
   def new
@@ -25,6 +36,20 @@ class Workspaces::DatasetsController < ApplicationController
   def show
     @dataset = @workspace.datasets.find params[:id]
     @comments = @dataset.comments.all
+  end
+
+  def edit
+    @dataset = @workspace.datasets.find params[:id]
+  end
+
+  def update
+    @dataset = @workspace.datasets.find params[:id]
+    if @dataset.update_attributes dataset_params
+      redirect_to workspace_dataset_path(@workspace, @dataset)
+    else
+      flash[:alert] = 'Could not update your dataset. Contact Admin'
+      render "edit"
+    end
   end
 
   private
