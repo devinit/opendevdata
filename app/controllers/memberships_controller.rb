28e0ed8a8de448c585b15ec453946b7cce28eedb
miclovich <vicmiclovich@gmail.com>
2014-06-23 02:22:49 +0300

    add membership controller

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,12 @@
+class MembershipsController < ApplicationController
+  def approve
+    @membership = Membership.find params[:id]
+    @membership.approved = true
+    @membership.save
+
+    respond_to do |format|
+      format.js
+      format.html { redirect_to [:workspaces, :pending], notice: "You've successfully approved #{@membership.user.first_name}'s membership." }
+    end
+  end
+end
