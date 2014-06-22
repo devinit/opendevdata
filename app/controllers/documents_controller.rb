168df75815317e693868028be0d785d611e70206
miclovich <vicmiclovich@gmail.com>
2014-06-23 02:18:01 +0300

    some simple access control for document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,13 +1,7 @@
 class DocumentsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
   before_filter :get_document, only: [:edit, :show, :update, :destroy]
-
-  def get_document
-    @document = Document.find(slug=params[:id])  # we are using slugs!
-    rescue Mongoid::Errors::DocumentNotFound
-      flash[:alert] = "The document you were looking for could not be found."
-      redirect_to documents_path
-  end
+  before_filter :ensure_has_access, only: [:edit, :update, :destroy]
 
   def index
     if params[:search].nil?
@@ -50,5 +44,20 @@ class DocumentsController < ApplicationController
   private
   def document_params
     params.require(:document).permit(:name, :description, :attachment, :tags)
+  end
+
+  def get_document
+    @document = Document.find(slug=params[:id])  # we are using slugs!
+    rescue Mongoid::Errors::DocumentNotFound
+      flash[:alert] = "The document you were looking for could not be found."
+      redirect_to documents_path
+  end
+
+  def ensure_has_access
+    if !@document.user.nil? or !@document.workspace.nil?
+      if !(@document.user == current_user) or !@document.workspace.memberships.where(user: current_user).exists()
+        redirect_to root_path, alert: "You don't have permission to do this. You have to either belong to workspace or be owner of document to make such a change"
+      end
+    end
   end
 end
