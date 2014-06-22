36239737af3798efe7e08e54f0b50922eda5123c
miclovich <vicmiclovich@gmail.com>
2014-06-22 22:01:51 +0300

    commented out

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,8 +2,9 @@ class Membership
   include Mongoid::Document
   field :admin, type: Boolean, default: false
   field :approved, type: Boolean, default: false
+
   belongs_to :user, dependent: :destroy
   belongs_to :workspace, dependent: :destroy
 
-  validates_uniqueness_of :user_id
+  # validates_uniqueness_of :user_id # TODO -> figure out a better validation!
 end
