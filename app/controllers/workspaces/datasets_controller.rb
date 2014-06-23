bee89d62486c7b867b8fcfb5d5a3f5703283f694
miclovich <vicmiclovich@gmail.com>
2014-06-23 04:07:18 +0300

    stub code...wip

--- app/controllers/workspaces/datasets_controller.rb
+++ app/controllers/workspaces/datasets_controller.rb
@@ -49,6 +49,36 @@ class Workspaces::DatasetsController < ApplicationController
   def show
     @dataset = @workspace.datasets.find params[:id]
     @comments = @dataset.comments.all
+    data_exract = @dataset.data_extract
+    if @dataset.data_extract
+      gon.data_values ||= @dataset.data_extract['data_values']
+      gon.headings_gon ||= @dataset.data_extract['headings']
+      gon.chartTitle ||= @dataset.title
+      gon.subTitle ||= @dataset.sub_title
+      gon.yLabel ||= @dataset.y_label
+      gon.xLabel ||= @dataset.x_label
+      gon.data_units ||= @dataset.data_units
+    end
+    @chart_type ||= @dataset.chart_type
+    if @chart_type == ""
+      @chart_type = nil
+    end
+
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(:text => @dataset.title)
+      f.xAxis(:categories => ["United States", "Japan", "China", "Germany", "France"])
+      f.series(:name => "GDP in Billions", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
+      f.series(:name => "Population in Millions", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
+
+      f.yAxis [
+        {:title => {:text => "GDP in Billions", :margin => 70} },
+        {:title => {:text => "Population in Millions"}, :opposite => true},
+      ]
+
+      f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+      f.chart({:defaultSeriesType=>"column"})
+    end
+
   end
 
   def edit
