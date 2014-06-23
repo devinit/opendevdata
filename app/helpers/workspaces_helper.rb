167468a9ba2216e69afe329a35a5fbdb9075965c
miclovich <vicmiclovich@gmail.com>
2014-06-23 16:53:40 +0300

    refactor helper

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -1,10 +1,15 @@
 module WorkspacesHelper
   def has_access? workspace, user
-    workspace.memberships.where(user: user, approved: true).exists?
+    if workspace.nil?
+      false
+    else
+      workspace.memberships.where(user: user, approved: true).exists?
+    end
   end
 
   def has_change_access? workspace, user
     # user allowed to do danger changes
     workspace.memberships.where(user: user, admin: true).exists?
   end
+
 end
