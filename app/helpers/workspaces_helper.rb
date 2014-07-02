404d536ea45947999919bddcf9f56b404eea5b86
miclovich <vicmiclovich@gmail.com>
2014-07-03 00:27:41 +0300

    Улучшенная удобно рабочее пространство помощник

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -1,10 +1,18 @@
 module WorkspacesHelper
-  def has_access? workspace, user
-    if workspace.nil?
+  def has_access? workspace, user, dataset=nil
+    if dataset
+      # hate duplication!
+      current_user == dataset.user or current_user.is_admin?
+    elsif workspace.nil?
       false
     else
-      workspace.memberships.where(user: user, approved: true).exists?
+      if signed_in? and current_user.is_admin?
+        true
+      else
+        workspace.memberships.where(user: user, approved: true).exists?
+      end
     end
+
   end
 
   def has_change_access? workspace, user
