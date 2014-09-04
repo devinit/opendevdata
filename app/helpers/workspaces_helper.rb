006c96a2efbf889f2b22dddc1cd302759e868d9d
miclovich <vicmiclovich@gmail.com>
2014-09-04 23:37:09 +0300

    cleanup

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -1,18 +1,20 @@
 module WorkspacesHelper
+
   def has_access? workspace, user, dataset=nil
+    state = false
     if dataset
       # hate duplication!
-      current_user == dataset.user or current_user.is_admin?
+      state = (current_user == dataset.user) or current_user.is_admin?
     elsif workspace.nil?
-      false
+      state = false
     else
       if signed_in? and current_user.is_admin?
-        true
+        state = true
       else
-        workspace.memberships.where(user: user, approved: true).exists?
+        state = workspace.memberships.where(user: user, approved: true).exists?
       end
     end
-
+    state
   end
 
   def has_change_access? workspace, user
