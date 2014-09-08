cdc5aae7be8c8f9b76f706d9bef0add1bbf7369a
miclovich <vicmiclovich@gmail.com>
2014-09-08 08:26:24 +0300

    not using

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User
   devise :database_authenticatable, :registerable, :recoverable,
     :rememberable, :trackable, :validatable, :confirmable
 
-  devise :omniauthable, omniauth_providers: [:facebook, :twitter]
+  # devise :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   ## More user information
   field :first_name,         type: String
@@ -129,13 +129,13 @@ class User
     user
   end
 
-  def self.new_with_session params, session
-    super.tap do |user|
-      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
-        user.email = data['email'] if user.email.blank?
-      end
-    end
-  end
+  # def self.new_with_session params, session
+  #   super.tap do |user|
+  #     if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+  #       user.email = data['email'] if user.email.blank?
+  #     end
+  #   end
+  # end
 
   protected
     def set_name_and_case
