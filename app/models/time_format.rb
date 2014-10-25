a98b0b5d2bb387cfadf01e107c0a088a1e9d90cc
miclovich <vicmiclovich@gmail.com>
2014-10-25 22:46:09 +0300

    add uniqueness and time stamps

--- app/models/time_format.rb
+++ app/models/time_format.rb
@@ -1,9 +1,13 @@
 class TimeFormat
   include Mongoid::Document
   include Mongoid::Slug
+  include Mongoid::Timestamps
 
   field :code, type: String
   field :description, type: String
+  validates :code, presence: true, uniqueness: true
+  validates :description, presence: true
+
   # Some possible values (Admin should input this)
   # ==============================================
   # Code | Description
