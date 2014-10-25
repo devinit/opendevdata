5af8b3f851640c0f0bd33393d07d9e6a3170cce6
miclovich <vicmiclovich@gmail.com>
2014-10-25 22:45:34 +0300

    validate uniqueness

--- app/models/unit_of_measure.rb
+++ app/models/unit_of_measure.rb
@@ -1,9 +1,14 @@
 class UnitOfMeasure
   include Mongoid::Document
   include Mongoid::Slug
+  include Mongoid::Timestamps
 
   field :code, type: String
   field :description, type: String
+
+  validates :code, presence: true, uniqueness: true
+  validates :description, presence: true
+
   # Some possible values (Admin should input this)
   # ==============================================
   # Code | Description
