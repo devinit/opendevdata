108316c58977f2a1a113c243099dc025347d79e5
miclovich <vicmiclovich@gmail.com>
2014-10-25 22:48:52 +0300

    cleanup

--- app/models/joined_up_dataset.rb
+++ app/models/joined_up_dataset.rb
@@ -2,9 +2,10 @@ class JoinedUpDataset
   include Mongoid::Document
   include Mongoid::Slug
   include Mongoid::Taggable
+  include Mongoid::Timestamps
   include PublicActivity::Model
   tracked
-  before_save :set_time
+  # before_save :set_time
 
   field :created_at, type: Time
   field :edited_at, type: Time
@@ -12,24 +13,25 @@ class JoinedUpDataset
 
   # TODO -> scope out joined up data
   field :time_value, type: String
-  field :time_format, type: String
+  # field :time_format, type: String
   field :value, type: BigDecimal  # convert integers to decimals too ;-)
 
+  has_one :time_format
   has_one :space_value # related to space_value
   has_one :data_serie # related to data series
 
   # validations
-  validates :time_format, inclusion: { in: %w(year quarter month)}
+  validates :time_value, inclusion: { in: %w(year quarter month)}
 
   mount_uploader :attachment, DatasetFileUploader
 
-  protected
-  def set_time
-    if created_at.nil?
-      self.created_at = Time.now
-    else
-      self.edited_at = Time.now  # update the edited time
-    end
-  end
+  # protected
+  # def set_time
+  #   if created_at.nil?
+  #     self.created_at = Time.now
+  #   else
+  #     self.edited_at = Time.now  # update the edited time
+  #   end
+  # end
 
 end
