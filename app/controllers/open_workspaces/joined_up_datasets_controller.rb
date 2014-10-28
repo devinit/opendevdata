36dc0379dfd9bc03bc74bd80c0c8c50f9eac2f31
miclovich <vicmiclovich@gmail.com>
2014-10-28 10:37:52 +0300

    add actions

--- app/controllers/open_workspaces/joined_up_datasets_controller.rb
+++ app/controllers/open_workspaces/joined_up_datasets_controller.rb
@@ -1,6 +1,16 @@
 class OpenWorkspaces::JoinedUpDatasetsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def upload
+  end
+
+  def show
+    @joined_up_dataset = JoinedUpDataset.find params[:id]
+  end
+
+  def index
+    @joined_up_datasets = JoinedUpDataset.all
   end
 
 
