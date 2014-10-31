70b008742c8a5168d99f6fbe6bbe032dcb6abc69
miclovich <vicmiclovich@gmail.com>
2014-10-31 16:30:05 +0300

    add docs, pending action, destroy and update stubs

--- app/controllers/open_workspaces/joined_up_datasets_controller.rb
+++ app/controllers/open_workspaces/joined_up_datasets_controller.rb
@@ -2,17 +2,24 @@ class OpenWorkspaces::JoinedUpDatasetsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_workspace
 
+  # Some differences with how we are working with REST
+  # - upload (a user will put in a CSV or use their existing excel utilities
+  # to convert existing data to a CSV format (hopefully it will be standard
+  # - to update a dataset, user basically has to attach a newer CVS file or
+  # Delete the joined up dataset)
   def upload
   end
 
   def import
-    logger.debug ">>> attempting import"
     JoinedUpDataset.import params[:file], current_user, @workspace
-    logger.debug "<<< COMPLETE"
-    redirect_to open_workspace_processing_url
+    redirect_to open_workspace_processing_url(@workspace)
   end
 
   def processing
+  end
+
+  def pending
+    @pending = JoinedUpDataset.where pending: true
   end
 
   def show
@@ -21,6 +28,12 @@ class OpenWorkspaces::JoinedUpDatasetsController < ApplicationController
 
   def index
     @joined_up_datasets = JoinedUpDataset.all
+  end
+
+  def destroy
+  end
+
+  def update
   end
 
   private
