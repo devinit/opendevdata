82cd864eb0694bcdfce6f787557190688268af1e
miclovich <vicmiclovich@gmail.com>
2014-10-31 16:08:29 +0300

    add approved boolean, data_extract hash...update import function TODO

--- app/models/joined_up_dataset.rb
+++ app/models/joined_up_dataset.rb
@@ -17,6 +17,9 @@ class JoinedUpDataset
   # field :time_format, type: String
   field :value, type: BigDecimal  # convert integers to decimals too ;-)
 
+  field :data_extract, type: Hash
+  field :approved, type: Boolean, default: false
+
   has_one :time_format
   has_one :space_value # related to space_value
   has_one :data_serie # related to data series
@@ -24,14 +27,25 @@ class JoinedUpDataset
   belongs_to :open_workspace
   belongs_to :user  # who uploaded
 
-  # validations
-  validates :time_value, inclusion: { in: %w(year quarter month)}
+  # TODO -> do post save validations
+  # validates :time_value, inclusion: { in: %w(year quarter month)}
 
+  # Attachment that will be produced is something that is in the JoinedUpData format
   mount_uploader :attachment, DatasetFileUploader
 
-  def self.import file
+  def self.import file, user, open_workspace
     values_extracted = SmarterCSV.process(file.path)
-    logger.debug "values >> #{values_extracted.inspect}"
+    # headers = values_extracted[0].keys.map { |i| i.to_s } if values_extracted.size > 0
+    # create a joined up dataset (with nil values + attribute it to the uploader and the workspace that sent it.)
+    # TODO -> move operation to Sidekiq job
+    joined_up_dataset = JoinedUpDataset.new
+    joined_up_dataset.data_extract = { value_extract: values_extracted }
+    joined_up_dataset.user = user
+    joined_up_dataset.open_workspace = open_workspace
+    # logger.debug "values >> #{headers}"
+    if joined_up_dataset.save
+      logger.debug "Saved pre-liminary dataset"
+    end
   end
 
   # protected
