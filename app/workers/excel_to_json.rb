6064c5a8b7e07d6b35fefa238ffc3652707987d6
miclovich <vicmiclovich@gmail.com>
2014-10-31 16:08:48 +0300

    use logger

--- app/workers/excel_to_json.rb
+++ app/workers/excel_to_json.rb
@@ -5,27 +5,27 @@ class ExcelToJson
   include Sidekiq::Worker
   sidekiq_options retry: false
   def perform dataset_id, tempfile_path, chart_type, file_name
-    puts "---- ***** ---- *** ---- "
+    logger.debug "---- ***** ---- *** ---- "
     # test_link = 'http://localhost:8000/upload/'  # uncomment to test
     conn = Faraday.new url: 'http://e2j.opendevdata.ug/upload/' do |faraday|
     # conn = Faraday.new url: test_link do |faraday| # uncomment to test
       faraday.request :multipart
       faraday.adapter :net_http
     end
-    puts "-- connection object created..."
+    logger.debug "-- connection object created..."
     payload = { upload_file: Faraday::UploadIO.new(tempfile_path,
                                                    'application/vnd.ms-excel'),
                 chart_type: chart_type }
-    puts "--** Payload prepared... posting to connection"
+    logger.debug "--** Payload prepared... posting to connection"
 
     response = conn.post 'http://e2j.opendevdata.ug/upload/', payload
     # response = conn.post test_link, payload  # test
 
     if response.status == 200  #HTTP OK
       dataset = Dataset.find _id = dataset_id
-      puts "#{dataset.name} processed"
+      logger.debug "#{dataset.name} processed"
       json_response = response.body
-      puts "Updating dataset ***"
+      logger.debug "Updating dataset ***"
       dataset.data_extract = JSON.parse json_response
       dataset.save
     else
