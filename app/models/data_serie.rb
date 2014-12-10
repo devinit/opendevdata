db2bc356f00b68ed4de9db7d4e30a889d88a1942
miclovich <vicmiclovich@gmail.com>
2014-12-10 19:35:05 +0300

    change attribute to `name`

--- app/models/data_serie.rb
+++ app/models/data_serie.rb
@@ -3,13 +3,13 @@ class DataSerie
   include Mongoid::Timestamps
   include Mongoid::Slug
 
-  field :title, type: String
+  field :name, type: String
   field :description, type: String
   field :notes, type: String
   has_one :unit_of_measure
   has_one :sector_vocabulary
 
-  validates :title, :description, presence: true
+  validates :name, :description, presence: true
 
   has_many :joined_up_datasets
 
