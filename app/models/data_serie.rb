36d2f116b339f8af67b2f82d6800ec0038672521
miclovich <vicmiclovich@gmail.com>
2014-12-12 14:17:19 +0300

    add slug

--- app/models/data_serie.rb
+++ app/models/data_serie.rb
@@ -7,7 +7,7 @@ class DataSerie
   field :name, type: String
   field :description, type: String
   field :notes, type: String
-
+  slug :name
   # has_one :unit_of_measure
   # has_one :sector_vocabulary
 
