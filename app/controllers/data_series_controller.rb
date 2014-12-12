10c401825af906f568cdfc17313078f230d4b593
miclovich <vicmiclovich@gmail.com>
2014-12-12 13:44:57 +0300

    fix create action

--- app/controllers/data_series_controller.rb
+++ app/controllers/data_series_controller.rb
@@ -1,4 +1,6 @@
 class DataSeriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
@@ -9,5 +11,27 @@ class DataSeriesController < ApplicationController
   end
 
   def new
+    @data_serie = DataSerie.new
   end
+
+  def create
+    @data_serie = DataSerie.create data_series_params
+
+    respond_to do |format|
+      if @data_serie.save
+        format.html { redirect_to @data_serie, notice: "Data serie has been created"}
+        format.json { render json: @data_serie, status: :created, location: @data_serie }
+      else
+        format.html { render :action => "new"}
+        format.json { render json: @data_serie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    def data_series_params
+      params.require(:data_serie).permit(:name, :description)
+    end
+
 end
