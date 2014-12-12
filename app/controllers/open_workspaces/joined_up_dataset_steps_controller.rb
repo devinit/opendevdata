f0d98b9abb63f7bab1ada3b5d0babc20a19d3196
miclovich <vicmiclovich@gmail.com>
2014-12-12 14:31:56 +0300

    add default path

--- app/controllers/open_workspaces/joined_up_dataset_steps_controller.rb
+++ app/controllers/open_workspaces/joined_up_dataset_steps_controller.rb
@@ -21,6 +21,10 @@ class OpenWorkspaces::JoinedUpDatasetStepsController < ApplicationController
     render_wizard @joined_up_dataset
   end
 
+  def finish_wizard_path
+    open_workspace_joined_up_dataset_path(@workspace, current_joined_up_dataset.id)
+  end
+
   private
     def joined_up_dataset_params
       params.require(:joined_up_dataset).permit(:name, :source_of_data)
