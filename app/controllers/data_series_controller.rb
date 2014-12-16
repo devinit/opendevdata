eee3c786d7eeb3b64f70cef3c100edce04806b56
miclovich <vicmiclovich@gmail.com>
2014-12-16 16:23:52 +0300

    give a better nasty error

--- app/controllers/data_series_controller.rb
+++ app/controllers/data_series_controller.rb
@@ -1,5 +1,5 @@
 class DataSeriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :create_endpoint
 
   def index
   end
@@ -15,15 +15,20 @@ class DataSeriesController < ApplicationController
   end
 
   def create_endpoint
-    name = params[:name]
-    description = params[:description]
-    @data_serie = DataSerie.new
-    @data_serie.name = name
-    @data_serie.description = description
-    if @data_serie.save
-      render json: @data_serie, status: :created, location: @data_serie
+    if signed_in?
+      name = params[:name]
+      description = params[:description]
+      logger.info "OVER HERE"
+      @data_serie = DataSerie.new
+      @data_serie.name = name
+      @data_serie.description = description
+      if @data_serie.save
+        render json: @data_serie, status: :created, location: @data_serie
+      else
+        render json: @data_serie.errors, status: :unprocessable_entity
+      end
     else
-      render json: @data_serie.errors, status: :unprocessable_entity
+      render json: {error: "You can't POST here unless you have signed in!"}
     end
   end
 
