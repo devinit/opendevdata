d4e9de7a25bbb69e41689fdf20aaaeca08fdae36
miclovich <vicmiclovich@gmail.com>
2014-12-25 05:35:47 +0300

    re-write tests pending

--- spec/controllers/joined_up_datasets_steps_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe JoinedUpDatasetsStepsController, :type => :controller do
-
-end
--- spec/controllers/open_workspaces/joined_up_dataset_steps_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe OpenWorkspaces::JoinedUpDatasetStepsController, :type => :controller do
-
-end
