f58069be2f9d81bc744bd69f14a40b35d7574c05
victor miclovich <vicmiclovich@gmail.com>
2015-02-06 14:05:19 -0800

    tweak locator spell a bit

--- app/models/locator.rb
+++ app/models/locator.rb
@@ -1,4 +1,4 @@
-# Just called this locator! :-) 
+# Just called this locator! :-)
 
 class Locator
 	include Mongoid::Document
@@ -8,11 +8,21 @@ class Locator
 
 	field :name, type: String
 
+	# Specific/experimment fields
+	# Village Code,Village,Parish Code,Parish,Sub-County Code,Sub-County,District Code,District
+	field :village_code, type: String
+	field :village, type: String
+	field :parish_code, type: String
+	field :subcounty, type: String
+	field :subcounty_code, type: String
+	field :district, type: String
+	field :district_code, type: String
+
 	# geometry type
 	# field :location, type: Point
 	geo_field :location # indexes too
 
 	# field :route, type: Linestring #TOFIX -> Linestring unrecognizable
 	field :area, type: Polygon
-	belongs_to :location_type	
+	belongs_to :location_type
 end
