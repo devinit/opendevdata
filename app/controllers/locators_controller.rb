33752fb590cd483b269d2e83e0aae23e8c3ec885
victor miclovich <vicmiclovich@gmail.com>
2015-02-06 16:49:02 -0800

    change redirect

--- app/controllers/locators_controller.rb
+++ app/controllers/locators_controller.rb
@@ -3,6 +3,6 @@ class LocatorsController < ApplicationController
   def generate_geo_records
     possible_names = ['district', 'parish', 'region', 'subcounty', 'village']
     GenerateGeoData.perform_async possible_names
-    redirect_to admin_path, alert: 'Your geo-records are being reprocessed'
+    redirect_to admin_path, notice: 'Your geo-records are being reprocessed'
   end
 end
