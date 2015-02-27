3f79d8d0e06fca69cbea68d26b3d2573ae2037c9
victor miclovich <vicmiclovich@gmail.com>
2015-02-27 10:27:39 -0800

    auto generate units of measure

--- app/controllers/locators_controller.rb
+++ app/controllers/locators_controller.rb
@@ -1,8 +1,16 @@
 class LocatorsController < ApplicationController
 
   def generate_geo_records
+    # Worst place to do all this magic, but makes sense to
+    # onboard data that is static once
     possible_names = ['district', 'parish', 'region', 'subcounty', 'village']
-    GenerateGeoData.perform_async possible_names
+    units_of_measure = [
+      ['U', 'Unit'],
+      ['PC', "Percent"],
+      ["UGXC", "Uganda Shillings (Current)"],
+      ["USDC", "US Dollars (Current)"]
+    ]
+    GenerateStaticData.perform_async possible_names, units_of_measure
     redirect_to admin_path, notice: 'Your geo-records are being reprocessed'
   end
 end
