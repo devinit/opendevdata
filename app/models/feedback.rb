4d049cb9a637c6d0588ff99aa8957cda3ac28e34
victor miclovich <vicmiclovich@gmail.com>
2015-05-12 14:36:49 +0300

    add feedback model

---
+++ app/models/feedback.rb
@@ -0,0 +1,12 @@
+class Feedback
+  include Mongoid::Document
+  include SimpleEnum::Mongoid
+  field :country, type: String
+  field :remarks, type: String
+  field :first_name, type: String
+  field :last_name, type: String
+  # field :gender, type: String
+  as_enum :gender, female: 1, male: 0
+
+  belongs_to :open_workspace
+end
