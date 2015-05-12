935efac62304cce3fa7159149d7a856a4a29e359
victor miclovich <vicmiclovich@gmail.com>
2015-05-12 14:51:43 +0300

    add missing a

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,4 @@
-Recaptch.configure do |config|
+Recaptcha.configure do |config|
   config.public_key = "6LcxvAYTAAAAAPr4cZIFZPtXi0M5vc8g9GVhy-fl"
   config.private_key = "6LcxvAYTAAAAAPr4cZIFZPtXi0M5vc8g9GVhy-fl"
-
 end
